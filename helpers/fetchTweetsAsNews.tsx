c9220c9c6c07e706a8f26a571829827d28e1ce05
German Meza <iam@germanamz.com>
2022-10-24 10:03:32 -0500

    feat: boundaries for external services

--- helpers/fetchTweetsAsNews.tsx
+++ helpers/fetchTweetsAsNews.tsx
@@ -28,23 +28,33 @@ type ApiRes = {
 };
 
 const fetchTweetsAsNews = async (query: string): Promise<News[]> => {
-  const headers = new Headers({
-    Authorization: `Bearer ${process.env.TWITTER_BREARER}`,
-  });
-  // @ts-ignore
-  const res = await fetch(`https://api.twitter.com/2/tweets/search/recent?query=${query}&expansions=author_id&max_results=20`, {
-    method: 'GET',
-    headers,
-  });
-  const apiRes: ApiRes = await res.json();
-  const authors: Record<string, ApiUser> = apiRes.includes.users.reduce((acc, author) => ({ ...acc, [author.id]: author}), {});
+  try {
+    const headers = new Headers({
+      Authorization: `Bearer ${process.env.TWITTER_BREARER}`,
+    });
+    const res = await fetch(`https://api.twitter.com/2/tweets/search/recent?query=${query}&expansions=author_id&max_results=20`, {
+      method: 'GET',
+      headers,
+    });
 
-  return apiRes.data.map((apiTweet) => ({
-    title: apiTweet.text,
-    subtitle: `@${authors[apiTweet.author_id].username}`,
-    blank: true,
-    target: `https://twitter.com/${authors[apiTweet.author_id].username}/status/${apiTweet.id}`,
-  }));
+    if (!res.ok) {
+      console.error(await res.text());
+      return [];
+    }
+
+    const apiRes: ApiRes = await res.json();
+    const authors: Record<string, ApiUser> = apiRes.includes.users.reduce((acc, author) => ({ ...acc, [author.id]: author}), {});
+
+    return apiRes.data.map((apiTweet) => ({
+      title: apiTweet.text,
+      subtitle: `@${authors[apiTweet.author_id].username}`,
+      blank: true,
+      target: `https://twitter.com/${authors[apiTweet.author_id].username}/status/${apiTweet.id}`,
+    }));
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 };
 
 export default fetchTweetsAsNews;
--- helpers/fetchYcomBestStoriesAsNews.ts
+++ helpers/fetchYcomBestStoriesAsNews.ts
@@ -20,28 +20,39 @@ const getStory = async (id: number): Promise<YcomStory> => {
 };
 
 const fetchYcomBestStoriesAsNews = async (numOfStories: number = 20): Promise<News[]> => {
-  const storiesReq = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
-  const storiesIds = await storiesReq.json();
-  const storyPromises = [];
+  try {
+    const storiesReq = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
 
-  for (let idIndex = 0; idIndex < numOfStories; idIndex += 1) {
-    const storyId = storiesIds[idIndex];
-    storyPromises.push(getStory(storyId));
-  }
-
-  const stories: YcomStory[] = await Promise.all(storyPromises);
-
-  return stories.map((story) => {
-    const news: News = {
-      title: story.title,
-      subtitle: story.by,
-      blank: true,
-    };
-    if (story.url) {
-      news.target = story.url;
+    if (!storiesReq.ok) {
+      console.error(await storiesReq.text());
+      return [];
     }
-    return news;
-  });
+
+    const storiesIds = await storiesReq.json();
+    const storyPromises = [];
+
+    for (let idIndex = 0; idIndex < numOfStories; idIndex += 1) {
+      const storyId = storiesIds[idIndex];
+      storyPromises.push(getStory(storyId));
+    }
+
+    const stories: YcomStory[] = await Promise.all(storyPromises);
+
+    return stories.map((story) => {
+      const news: News = {
+        title: story.title,
+        subtitle: story.by,
+        blank: true,
+      };
+      if (story.url) {
+        news.target = story.url;
+      }
+      return news;
+    });
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
 };
 
 export default fetchYcomBestStoriesAsNews;
