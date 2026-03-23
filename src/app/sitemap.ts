5c07431b17aa5c7ca265feb8a743a9cac49f126c
German Meza <iam@germanamz.com>
2026-03-23 06:29:38 -0600

    feat: update sitemap to include blog post URLs dynamically

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,15 @@
 import { MetadataRoute } from 'next';
+import { getAllPosts } from '@/lib/posts';
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://germanamz.com';
+
+  const postEntries: MetadataRoute.Sitemap = getAllPosts().map((post) => ({
+    url: `${baseUrl}/writing/${post.slug}`,
+    lastModified: new Date(post.date),
+    changeFrequency: 'monthly',
+    priority: 0.6,
+  }));
 
   return [
     {
@@ -22,5 +30,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 0.7,
     },
+    ...postEntries,
   ];
 }
